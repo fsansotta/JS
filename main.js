51772bdab39321fc336f8c796b04a58906ee9739
fsansotta <sansottafariel@gmail.com>
2023-11-02 20:30:19 -0300

    agrego template de card y resumen carrito

--- main.js
+++ main.js
@@ -2,36 +2,38 @@ const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".botones-productos");
 const carritoCompras = document.getElementById('resumenCarrito')
 const header = document.getElementById("header");
-let total = 0
-let carrito = [];
+const divProductos = document.querySelector('.container')
+const btn = document.getElementById('btnVaciarCarrito')
+let carrito = localStorage.getItem('carrito') ? JSON.parse(localStorage.getItem('carrito')) : [];
+let total = localStorage.getItem('precioTotal') ? JSON.parse(localStorage.getItem('precioTotal')) : 0;
+
 
 const productosArray = [
-    { id: 1, nombre: "Titular 2023", precio: 45000, imagen: "./imagenes/camisetas/PU771561-02-1.jpg" },
-    { id: 2, nombre: "Suplente 2023", precio: 40000, imagen: "./imagenes/camisetas/PU771554-01-1.jpg" },
-    { id: 3, nombre: "Suplente 2021", precio: 30000, imagen: "./imagenes/camisetas/PU_759748-01-1.jpg" },
-    { id: 4, nombre: "Titular 94", precio: 38000, imagen: "./imagenes/camisetas/Captura de pantalla 2023-10-29 184028.png" },
-    { id: 5, nombre: "Suplente 2016", precio: 20000, imagen: "./imagenes/camisetas/56cbaa88951af-360117-500x500.jpg" },
+    { id: 1, nombre: "Titular 2023", precio: 45000, imagen: "./imagenes/IMG-20231031-WA0194.png" },
+    { id: 2, nombre: "Suplente 2023", precio: 40000, imagen: "./imagenes/IMG-20231031-WA0195.png" },
+    { id: 3, nombre: "Suplente 2021", precio: 30000, imagen: "./imagenes/IMG-20231031-WA0192.png" },
+    { id: 4, nombre: "Titular 94", precio: 38000, imagen: "./imagenes/IMG-20231031-WA0193.png" },
+    { id: 5, nombre: "Suplente 2016", precio: 20000, imagen: "./imagenes/IMG-20231031-WA0196.png" },
     
 ]
 
 
 function cargarProductos() {
-    productosArray.forEach(producto => {
-
-        const div = document.createElement("div")
-        div.classList.add("producto");
-        div.innerHTML += ` <img class="producto-imagen" src="${producto.imagen}" alt="${producto.nombre}">
-        <div class="producto-detalles">
-            <h3 class="producto-titulo">${producto.nombre}</h3>
-            <p class="producto-precio">${producto.precio}</p>
-            <button type="button" class="btn btn-danger" onclick="agregarAlCarrito(${producto.id})">AGREGAR AL CARRITO</button>
-        </div>
-        `;
-
-        contenedorProductos.append(div);
-
+    productosArray.forEach(producto => {      
+        divProductos.innerHTML += `
+        <div class="card">
+                    <div class="imgBx">
+                        <img src="${producto.imagen}" alt="nike-air-shoe">
+                    </div>    
+                    <div class="contentBx">    
+                        <h4>${producto.nombre}</h4>    
+                        <h5>$${producto.precio}</h5>                        
+                        <button onclick='agregarAlCarrito (${producto.id})'>Agregar al carrito</button>
+                    </div>    
+                </div>
+        `;    
     })
-
+    detalleCarrito()
 }
 
 cargarProductos();
@@ -39,35 +41,28 @@ cargarProductos();
 
 function agregarAlCarrito(id) {
     const producto = productosArray.find(item => item.id === id);
+    const productoEnCarrito = carrito.find(item => item.id === id);
     total += producto.precio;
-    carrito.push(producto);
-    localStorage.setItem('carrito', JSON.stringify(carrito));
-    mostrarIcono();
+    if (productoEnCarrito) {
+        productoEnCarrito.cantidad++;
+    } else {
+        carrito.push({ ...producto, cantidad: 1 });
+    }
+    localStorage.setItem('carrito', JSON.stringify(carrito)); 
+    localStorage.setItem('precioTotal', JSON.stringify(total)); 
+    btn.style.display='block'
     detalleCarrito();
 }
 
 
-
-function mostrarIcono() {
-    if (carrito.length > 0) {
-        header.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
-    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-  </svg>
-`
-    }
-    else {
-        header.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-  </svg>`
-    }
-}
-
 function vaciarCarrito() {
     carrito = [];
-    localStorage.removeItem('carrito');
-    mostrarIcono();
+    localStorage.removeItem('carrito');  
+    localStorage.removeItem('precioTotal');  
     carritoCompras.innerHTML = ""
     total = 0
+    btn.style.display='none'
+    detalleCarrito()
 }
 
 
@@ -75,14 +70,11 @@ function detalleCarrito() {
     carritoCompras.innerHTML = "";
     carrito.forEach(element => {
         carritoCompras.innerHTML += `
-    <h6>${element.nombre}</h6>
-   
-    `
+            <h6>${element.nombre} - Cantidad: ${element.cantidad}</h6>
+        `;
     });
-    
-    const div = document.createElement("div")
-    div.innerHTML=`<h6> Total: $ ${total}</h6>`
+
+    const div = document.createElement("div");
+    div.innerHTML = `<h6> Total: $ ${total}</h6>`;
     carritoCompras.append(div);
-}
-
-
+   }
